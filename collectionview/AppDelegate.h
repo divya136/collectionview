d1c5b0c525baac3ba86049d1e6726f3d3436868c
Guna sundari <techforum136@gmail.com>
2017-06-02 13:05:04 +0530

    Initial Commit

---
+++ collectionview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  collectionview
+//
+//  Created by Guna Sundari on 02/06/17.
+//  Copyright © 2017 Guna Sundari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ collectionview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionview
+//
+//  Created by Guna Sundari on 02/06/17.
+//  Copyright © 2017 Guna Sundari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
